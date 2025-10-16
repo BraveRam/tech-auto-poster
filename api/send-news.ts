aa384c0ebbe6cc740e40e4aee656f54558e9f137
BraveRam <99811385+BraveRam@users.noreply.github.com>
2025-10-16 14:59:48 +0300

    Added more news source

--- api/send-news.ts
+++ api/send-news.ts
@@ -54,7 +54,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       const caption = `
 📰 <b>${article.title}</b>
 
-📝 ${article.description}
+📝 <i>${article.description}</i>
 
 🔗 <a href="${article.url}">Read more</a>
       `;
